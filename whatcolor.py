1c5e179ed363f9720524601c80c1f3e0243d0e29
DNissly <dnissly33@gmail.com>
2025-03-13 15:11:52 -0500

    updated to batch process and put results in CSV

--- whatcolor.py
+++ whatcolor.py
@@ -2,9 +2,10 @@ import joblib
 import os
 import cv2
 import numpy as np
+import pandas as pd
 
 #load the model
-knn_loaded = joblib.load("color_modelv1.pkl")
+knn_loaded = joblib.load("color_modelv2.pkl")
 print("Model loaded successfully!")
 
 #how to calc mean image color
@@ -25,13 +26,36 @@ def get_center_pixel_color(image_path):
     center_pixel = image[center_y, center_x]
     return center_pixel
 
-#ONLY CHOOSE ONE OF THE ABOVE METHODS (just func defs up here though)
+#ONLY CHOOSE ONE OF THE ABOVE METHODS (just func defs above here though)
 
 def predict_color(image_path, model):
     center_color = extract_color_features(image_path).reshape(1, -1)  # Reshape for prediction
     predicted_label = model.predict(center_color)
     return predicted_label[0]
 
-image_path = "test_image.jpg"
-predicted_color = predict_color(image_path, knn_loaded)
-print(f"Predicted Color: {predicted_color}")
+def process_images_from_folder(folder_path, model, output_file):
+    image_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.lower().endswith(('jpeg', 'jpg'))]
+    results = []
+    
+    for image_path in image_files:
+        image_name = os.path.basename(image_path)
+        image_name = image_name.partition('.')[0]
+        predicted_color = predict_color(image_path, model)
+        results.append((image_name, predicted_color))
+    
+    df = pd.DataFrame(results, columns=["Name", "Predicted Color"])
+    df.to_csv(output_file, index=False)
+    print(f"Results saved to {output_file}")
+
+###########################################
+
+# image_path = "test.jpg"
+# predicted_color = predict_color(image_path, knn_loaded)
+# print(f"Predicted Color: {predicted_color}")
+
+#single image test ^^^^^^^^
+#multi image test vvvvvvvv
+
+image_folder = "test/"  
+output_csv = "color_predictions.csv"
+process_images_from_folder(image_folder, knn_loaded, output_csv)
